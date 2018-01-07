e5cd7eb8e75a3abe8ac09eecd825c1eba44c0697
sudheer <sudhir23.mca@gmail.com>
2018-01-07 12:51:36 +0530

    sum_Proj

---
+++ Sum_Proj/src/main/java/Sum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Sum{
+	//int a = 10;
+	//int b = 20;
+	//int c = a+b;
+public static void main(String args[]){
+int a = 10;
+int b = 30;
+int c=a+b;
+System.out.println("Sum of numbers :"+ c);
+}
+}
